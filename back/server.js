a86bd0d1547557180cd39b7d9e10a5300ee5c96c
zhanggj <dinozhang9898@163.com>
2023-04-04 22:08:00 +0800

    fix

--- back/server.js
+++ back/server.js
@@ -1,7 +1,7 @@
 const moment = require ('moment')
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8089 });//设置端口
+const wss = new WebSocket.Server({ port: 8099 });//设置端口
 
 wss.on('connection', function connection(ws) {//对‘conntection’这个串进行监听，当收到这个后，执行下面的操作
   console.log('websocket已连接')

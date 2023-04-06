13bde059c6e17be31c49e0a26c74fa390ab29d4e
张国建 <dinozhang9898@163.com>
2023-04-06 17:04:30 +0800

    fix: 添加图片

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,19 +1,46 @@
 <template>
   <div class="about-wrapper">
-    <div v-for="item in imgs" :key="item">
-      <img :src="item" alt="">
+    <div class="swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide" v-for="item in imgs" :key="item">
+          <img :src="item" alt="">
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Swiper from 'swiper/dist/js/swiper.min.js';
+import 'swiper/dist/css/swiper.min.css';
   export default {
     data() {
       return {
         imgs: [
           require('@/assets/cat.jpg'), 
-          // require('@/assets/1132354.jpg')
+          require('@/assets/cat1.jpg'),
+          require('@/assets/cat2.jpg'), 
+          require('@/assets/cat3.jpg'), 
+          require('@/assets/cat4.jpg')
         ]
+      }
+    },
+    mounted() {
+      this.initSwiper();
+    },
+    methods: {
+      initSwiper() {
+        this.mySwiper = new Swiper('.swiper-container', {
+          autoplay: 5000,//可选选项，自动滑动
+          loop: true,
+          effect : 'cube',
+          cube: {
+            slideShadows: true,
+            shadow: true,
+            shadowOffset: 100,
+            shadowScale: 0.6
+          }
+        })
       }
     }
   }
@@ -21,9 +48,17 @@
 
 <style lang="scss" scoped>
 .about-wrapper {
-  img {
-    max-width: 100%;
-    max-height: 100%;
+  .swiper-container {
+    width: 500px;
+    height: 500px;
+    border-radius: 6px;
+    text-align: center;
+    background: #eee;
+    line-height: 500px;
+    img {
+      max-width: 100%;
+      max-height: 100%;
+    }
   }
 }
 </style>
